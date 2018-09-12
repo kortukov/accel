3feb180810b170ef3188b473eb988bbc92152eff
kortukov <elephunker1@gmail.com>
2018-09-12 12:18:46 +0300

    working hustle_timing script

---
+++ hustle_labels.m
@@ -0,0 +1,51 @@
+cd ../hustle_01
+files = dir('*.txt')
+
+
+for i = 1:size(files,1)
+    files(i).name
+    fid = fopen(files(i).name)
+    hustle_times = dlmread(files(i).name, ',',1,0)
+    hustle_times = hustle_times(:,1);
+    for j = 1:size(hustle_times,1)
+        hustle_times(j) = 60*(fix(hustle_times(j))) + 100*(hustle_times(j) - fix(hustle_times(j)));
+    end %перевели в секунды
+    fclose(fid)
+    %получили времена метаний
+    %надо теперь вшить их в данные после видео
+    k = strfind(files(i).name, '_') %здесь просто составляем имена необходимых файлов
+    folder_name = files(i).name(1:k(1)-1)
+    folder_name = ['../clean_data/',folder_name]
+    video_file_name = [files(i).name(1:k(2)-1),'_video_data.mat']
+    try
+        cd (folder_name)
+    catch
+        warning(['Video data not found, was searching for ',folder_name])
+        continue
+    end
+    load(video_file_name, '-mat')
+    %здесь нужно составить вектор hustle timecourse
+    %c единичками в тех таймингах timing где есть метания
+    %hustle_timecourse = zeros(size(stim_timecourse));
+    
+    %но я выбрал другой вариант - составить вектор
+    %hustle_timing - в котором номера кадров где есть метания
+    hustle_timing = []
+    j = 1;
+    k = 1;
+    while (j <= size(hustle_times,1) ) & (k <= size(timing,2))
+        %absarr = [absarr, abs(timing(k) - hustle_times(j))];
+        if abs(timing(k) - hustle_times(j)) > 0.05
+            k = k+1;
+        else
+            %hustle_timecourse(k) = 1
+            hustle_timing = [hustle_timing; k]
+            j = j+1;
+            k = k+1;
+        end
+    end
+    save(video_file_name, 'hustle_timing', '-append')
+    
+    cd /home/evgeny/lab/hustle_01
+end
+cd /home/evgeny/lab/task
