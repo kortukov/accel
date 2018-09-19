008f86fb5b57d89c7d1030ac3702e17d0c283d1d
kortukov <elephunker1@gmail.com>
2018-09-19 12:08:43 +0300

    synchro working only for correct videos

--- hustle_labels.m
+++ hustle_labels.m
@@ -4,13 +4,12 @@ files = dir('*.txt')
 
 for i = 1:size(files,1)
     files(i).name
-    fid = fopen(files(i).name)
     hustle_times = dlmread(files(i).name, ',',1,0)
     hustle_times = hustle_times(:,1);
     for j = 1:size(hustle_times,1)
         hustle_times(j) = 60*(fix(hustle_times(j))) + 100*(hustle_times(j) - fix(hustle_times(j)));
     end %перевели в секунды
-    fclose(fid)
+    
     %получили времена метаний
     %надо теперь вшить их в данные после видео
     k = strfind(files(i).name, '_') %здесь просто составляем имена необходимых файлов
@@ -19,11 +18,11 @@ for i = 1:size(files,1)
     video_file_name = [files(i).name(1:k(2)-1),'_video_data.mat']
     try
         cd (folder_name)
+        load(video_file_name, '-mat')
     catch
         warning(['Video data not found, was searching for ',folder_name])
         continue
     end
-    load(video_file_name, '-mat')
     %здесь нужно составить вектор hustle timecourse
     %c единичками в тех таймингах timing где есть метания
     %hustle_timecourse = zeros(size(stim_timecourse));
--- synchro.m
+++ synchro.m
@@ -1,63 +1,98 @@
+%Первый вариант алгоритма синхронизации
+%Рассчитываем по всем стимулам вектор разницы в тайминге ээг и видео
+%Не работает на файлах с выпадающими стимулам
 cd /home/evgeny/lab/ann
 files = dir('*.ann')
 
-for i = 4:4 %size(files,1)
-    markers = readtable(files(i).name, 'filetype', 'text')
-    markers = table2array(markers)
-    ST = [11,12,21,22,211,212,221,222]; %255 - конец записи
+corrupt_video_files = {} %Имена видео файлов, в которых потеряны стимулы
+for i = 1:size(files,1)
+    %считываем разметку даных 
+    cd /home/evgeny/lab/ann
+    markers = readtable(files(i).name, 'filetype', 'text');
+    markers = table2array(markers);
+    ST = [11,12,21,22,211,212,221,222, 255]; 
     RS = [101,104];
-    j = 1
-    while ~(ismember(markers(j,3), ST)) %ищем первый стимул
-        j = j + 1
-    end
-    first_st = markers(j,1)
     
     %теперь открываем видео-файлы
-    [path, filename, ext] = fileparts(files(i).name)
+    [path, filename, ext] = fileparts(files(i).name);
     k = strfind(filename, '_') %здесь просто составляем имена необходимых файлов
-    folder_name = filename(1:k(1)-1)
-    folder_name = ['../clean_data/',folder_name]
-    video_file_name = [filename,'_video_data.mat']
+    folder_name = filename(1:k(1)-1);
+    folder_name = ['../clean_data/',folder_name];
+    video_file_name = [filename,'_video_data.mat'];
     try
-        cd (folder_name)
-        load(video_file_name, 'stim_timecourse', 'timing', 'hustle_timing')
+        cd (folder_name);
+        load(video_file_name, 'stim_timecourse', 'timing');
     catch
         warning(['Video data not found, was searching for ',folder_name])
+        continue2
+    end
+    video_file_name
+    %смотрим когда были стимулы на ээг
+    %попутно помечаем эпохи где не было реакций
+    %тут есть защита от нескольких реакций в одной эпохе
+    no_reaction_epochs = [];
+    st_times_eeg = [];
+    rs_times_eeg = [];
+    j = 1;
+    while j < size(markers,1) %проходимся по всем строкам файла разметки
+        if ismember(markers(j,3),ST) %увидели стимул 
+            st_times_eeg = [st_times_eeg; markers(j,1)]; 
+            k = j + 1;
+            reaction = 0;
+            while not(ismember(markers(k, 3), ST))
+                if ismember(markers(k,3), RS)
+                    reaction = reaction + 1;
+                    if reaction == 1
+                        rs_times_eeg = [rs_times_eeg; markers(k,1)];
+                    end
+                end
+                k = k + 1;
+            end
+            if reaction == 0 %если не было реакции то пишем нолик в тайминг
+                rs_times_eeg = [rs_times_eeg; 0]
+                no_reaction_epochs = [no_reaction_epochs; size(rs_times_eeg, 1)]
+            end
+            j = k - 1; %чтобы не делать лишних итераций
+        end
+        j = j + 1;
+    end
+    save(video_file_name, 'no_reaction_epochs', '-append');
+    
+    %смотрим когда были стимулы на видео
+    st_times_vid = [];
+    for j = 1:size(stim_timecourse,1)
+         if stim_timecourse(j) == 1
+             st_times_vid = [st_times_vid; timing(j)];
+         end
+    end   
+    
+    if size(st_times_vid,1) ~= size(st_times_eeg,1)
+        corrupt_video_files{end + 1} = video_file_name;
         continue
     end
     
-    %считаем разницу в показаниях времени камеры и ээг
-    st_times_vid = []
-    for j = 1:size(stim_timecourse,1)
-         if stim_timecourse(j) == 1
-             st_times_vid = [st_times_vid; timing(j)]
-         end
+    %здесь считаем вектор разницы. К этому моменту
+    %должно быть гарантировано корректное совпадение стимулов
+    %на ээг и видео, иначе пишем плохой файл в лог и идём дальше
+    difference = zeros(size(st_times_vid));
+    for j = 1:size(st_times_vid) 
+        difference(j) = st_times_vid(j) - st_times_eeg(j)/1000;
     end
-    st_times_eeg = []
-    for j = 1:size(markers,1)
-        if ismember(markers(j,3), ST)
-            st_times_eeg = [st_times_eeg; markers(j,1)]
-        end
-    end
-    difference = zeros(size(st_times_vid))
-    for j = 1:size(st_times_vid)
-        difference(j) = st_times_vid(j) - st_times_eeg(j)/1000
-    end
-     
+    
     %составляем векторы тайминга стимулов и реакций
-    st_timing = [], rs_timing = [], k = 1
-    for j = 1:size(markers,1)
-        if ismember(markers(j,3), ST)
-            st_timing = [st_timing; markers(j,1)/1000 + difference(k)]
-        elseif ismember(markers(j,3), RS)
-            rs_timing = [rs_timing; markers(j,1)/1000 + difference(k)]
-            k = k+1
-        end
+    st_timing = zeros(size(st_times_eeg));
+    rs_timing = zeros(size(rs_times_eeg));
+    for j = 1:size(st_times_eeg)
+       st_timing(j) = st_times_eeg(j)/1000 + difference(j);
+       if ~(ismember(j, no_reaction_epochs))
+           rs_timing(j) = rs_times_eeg(j)/1000 + difference(j);
+       end
     end 
-    save(video_file_name, 'st_timing', 'rs_timing')
+    save(video_file_name, 'st_timing', 'rs_timing', '-append')
 end
 
 
-
+'thats it'
 cd /home/evgeny/lab/task
-
+save('corrupt_files.mat', 'corrupt_video_files', '-mat')
+'saved'
