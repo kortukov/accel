716321582f429a8aa78d9b41ebc94d0a87d54fcf
kortukov <elephunker1@gmail.com>
2018-11-09 15:46:02 +0300

    new checking tables

--- accel/checking_tables.m
+++ accel/checking_tables.m
@@ -2,31 +2,32 @@ cd /home/evgeny/lab/feat_tables_global
 
 files = dir('*.txt')
 
-table_accel_correction = {};
-table_accel_correction{1,1} = 'subject';
-table_accel_correction{1,2} = 'table';
-table_accel_correction{1,3} = 'accel';
-table_accel_correction{1,4} = 'correct';
-for i = 1:length(files)
+table_check = {};
+table_check{1,1} = 'subject';
+table_check{1,2} = 'trials';
+table_check{1,3} = 'hustles';
+table_check{1,4} = 'ratio';
+for i = 9%:length(files)
     cd /home/evgeny/lab/feat_tables_global
     filename = files(i).name
     table = readtable(filename);
-    table_columns = size(table,1);
-    k = strfind(filename, '_') 
-    accel_data_name = [filename(1:k(1)+1) '_accel_data.mat'];
-    cd /home/evgeny/lab/accel/data
-    try
-        load(accel_data_name);
-    catch
-        warning('Unable to load accel data');
-        table_accel_correction{i+1,1} = filename;
-        continue
-    end
-    accel_columns = length(data.data_matrix);
-    table_accel_correction{i+1,1} = filename;
-    table_accel_correction{i+1,2} = table_columns;
-    table_accel_correction{i+1,3} = accel_columns;
-    table_accel_correction{i+1,4} = accel_columns == table_columns;
+    
+    incorrectness = table.miss + table.badkey + table.early + table.late;
+    correctness = ~incorrectness;
+    trials = length(find(correctness));
+    
+    session = table.individual_time;
+    hustles = length(find(session > 400));
+    
+    ratio = hustles/trials;
+    
+    table_check{i+1,1} = filename;
+    table_check{i+1,2} = trials;
+    table_check{i+1,3} = hustles;
+    table_check{i+1,4} = ratio;
+    
+    
+    check = table_check()
     
 end
 
